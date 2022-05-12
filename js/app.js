6f0b474274b2478c63a9a92d0f5777258bb51341
Rui Guo <guojiarui@gmail.com>
2022-05-12 14:43:44 -0700

    js and readme.md updated

--- js/app.js
+++ js/app.js
@@ -20,53 +20,65 @@ function answerOne() {
 answerOne();
 
 
-
-let answerTwo = prompt('Has Steph ever thru hiked the CDT?').toLowerCase();
-
-if (answerTwo === 'yes' || answerTwo === 'y') {
-  //console.log('Fail');
-  alert('No, she thru hiked the Appalachian Trail in 2013. She would love to complete the CDT in the future though.');
-} else if (answerTwo === 'no' || answerTwo === 'n') {
-  //console.log('Pass');
-  score++;
-  alert('You are right! She was a NoBo AT thru hiker, and her trail name is Books!');
+function answerTwo() {
+  if (answerTwo === 'yes' || answerTwo === 'y') {
+    //console.log('Fail');
+    alert('No, she thru hiked the Appalachian Trail in 2013. She would love to complete the CDT in the future though.');
+  } else if (answerTwo === 'no' || answerTwo === 'n') {
+    //console.log('Pass');
+    score++;
+    alert('You are right! She was a NoBo AT thru hiker, and her trail name is Books!');
+  } let answerTwo = prompt('Has Steph ever thru hiked the CDT?').toLowerCase();
 }
+answerTwo();
 
-let answerThree = prompt('Does she have any wine or beer certifications?').toLowerCase();
 
-if (answerThree === 'yes' || answerThree === 'y') {
-  //console.log('Pass');
-  score++;
-  alert('Indeed! She is a Cicerone certified beer server and has her second level Wine & Spirit Education Trust certification.');
-} else if (answerThree === 'no' || answerThree === 'n') {
-  //console.log('Fail');
-  alert('It is surprising, but she is certified by Cicerone & WSET. Steph was pursuing a career in wine before she fell in love with coding.');
+
+function answerThree() {
+  let answerThree = prompt('Does she have any wine or beer certifications?').toLowerCase();
+
+  if (answerThree === 'yes' || answerThree === 'y') {
+    //console.log('Pass');
+    score++;
+    alert('Indeed! She is a Cicerone certified beer server and has her second level Wine & Spirit Education Trust certification.');
+  } else if (answerThree === 'no' || answerThree === 'n') {
+    //console.log('Fail');
+    alert('It is surprising, but she is certified by Cicerone & WSET. Steph was pursuing a career in wine before she fell in love with coding.');
+  }
 }
+answerThree();
 
-let answerFour = prompt('Has she biked the Camino de Santiago?').toLowerCase();
+function answerFour() {
+  let answerFour = prompt('Has she biked the Camino de Santiago?').toLowerCase();
 
-if (answerFour === 'yes' || answerFour === 'y') {
-  //console.log('Pass');
-  score++;
-  alert('Unfortunately yes, and she will be the first to tell you that it is not meant to be biked. If you are looking for a better route she recommends the Camino del Cid!');
-} else if (answerFour === 'no' || answerFour === 'n') {
-  //console.log('Fail');
-  alert('She admits it is a much better walking path, but she did in fact attempt this route on bike, which resulted in a lot of bike pushing!');
+  if (answerFour === 'yes' || answerFour === 'y') {
+    //console.log('Pass');
+    score++;
+    alert('Unfortunately yes, and she will be the first to tell you that it is not meant to be biked. If you are looking for a better route she recommends the Camino del Cid!');
+  } else if (answerFour === 'no' || answerFour === 'n') {
+    //console.log('Fail');
+    alert('She admits it is a much better walking path, but she did in fact attempt this route on bike, which resulted in a lot of bike pushing!');
+  }
 }
+answerFour();
 
-let answerFive = prompt('Is Steph a Master Diver?').toLowerCase();
-
-if (answerFive === 'yes' || answerFive === 'y') {
-  //console.log('Fail');
-  score++;
-  alert('She is not! Though she would love to be someday, she is currently only advanced open water with a few additional certifications.');
-} else if (answerFive === 'no' || answerFive === 'n') {
-  //console.log('Pass');
-  score++;
-  alert('You are correct, though she hopes to someday be certified and work on a live aboard, she is currently only PADI Advanced Open Water.');
-  //console.log(score);
-  //alert('Your score is ' + score);
+answerFive(){
+  if (answerFive === 'yes' || answerFive === 'y') {
+    //console.log('Fail');
+    score++;
+    alert('She is not! Though she would love to be someday, she is currently only advanced open water with a few additional certifications.');
+  } else if (answerFive === 'no' || answerFive === 'n') {
+    //console.log('Pass');
+    score++;
+    alert('You are correct, though she hopes to someday be certified and work on a live aboard, she is currently only PADI Advanced Open Water.');
+    //console.log(score);
+    //alert('Your score is ' + score);
+  }
+  let answerFive = prompt('Is Steph a Master Diver?').toLowerCase();
 }
+answerFive();
+
+
 
 let userQuestion = 'Which is my favorite book of these titles: The Overstory, Braiding Sweetgrass, Grapes of Wrath, On the Road, The Goldfinch, All the Light we Cannot See ';
 let answers = ['The Overstory', 'Braiding Sweetgrass', 'Grapes of Wrath', 'On the Road', 'The Goldfinch', 'All the Light we Cannot See'];
