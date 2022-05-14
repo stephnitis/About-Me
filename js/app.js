fc1ed37e8cd34aefdd9682250815c6b88a52b45b
Stephanie Hill <stephni.hill@gmail.com>
2022-05-13 20:58:48 -0700

    removed console logs and edited out code

--- js/app.js
+++ js/app.js
@@ -9,11 +9,10 @@ let score = 0;
 function answerOne() {
   let answerOne = prompt('Is Steph a certified yoga instructor?').toLowerCase();
   if (answerOne === 'yes' || answerOne === 'y') {
-    //console.log('Pass');
+
     score++;
     alert('Es verdad! She did a 500 hour certification in Rishikesh, as well as an internship in Medellin. She also keeps a yoga mat by her desk to stretch during breaks.');
   } else if (answerOne === 'no' || answerOne === 'n') {
-    //console.log('Fail');
     alert('Though she is not currently teaching she is still certified!');
   }
 }
@@ -22,10 +21,8 @@ answerOne();
 function answerTwo() {
   let answerTwo = prompt('Has Steph ever thru hiked the CDT?').toLowerCase();
   if (answerTwo === 'yes' || answerTwo === 'y') {
-    //console.log('Fail');
     alert('No, she thru hiked the Appalachian Trail in 2013. She would love to complete the CDT in the future though.');
   } else if (answerTwo === 'no' || answerTwo === 'n') {
-    //console.log('Pass');
     score++;
     alert('You are right! She was a NoBo AT thru hiker, and her trail name is Books!');
   }
@@ -35,11 +32,9 @@ answerTwo();
 function answerThree() {
   let answerThree = prompt('Does she have any wine or beer certifications?').toLowerCase();
   if (answerThree === 'yes' || answerThree === 'y') {
-    //console.log('Pass');
     score++;
     alert('Indeed! She is a Cicerone certified beer server and has her second level Wine & Spirit Education Trust certification.');
   } else if (answerThree === 'no' || answerThree === 'n') {
-    //console.log('Fail');
     alert('It is surprising, but she is certified by Cicerone & WSET. Steph was pursuing a career in wine before she fell in love with coding.');
   }
 }
@@ -48,11 +43,9 @@ answerThree();
 function answerFour() {
   let answerFour = prompt('Has she biked the Camino de Santiago?').toLowerCase();
   if (answerFour === 'yes' || answerFour === 'y') {
-    //console.log('Pass');
     score++;
     alert('Unfortunately yes, and she will be the first to tell you that it is not meant to be biked. If you are looking for a better route she recommends the Camino del Cid!');
   } else if (answerFour === 'no' || answerFour === 'n') {
-    //console.log('Fail');
     alert('She admits it is a much better walking path, but she did in fact attempt this route on bike, which resulted in a lot of bike pushing!');
   }
 }
@@ -61,15 +54,11 @@ answerFour();
 function answerFive() {
   let answerFive = prompt('Is Steph a Master Diver?').toLowerCase();
   if (answerFive === 'yes' || answerFive === 'y') {
-    //console.log('Fail');
     score++;
     alert('She is not! Though she would love to be someday, she is currently only advanced open water with a few additional certifications.');
   } else if (answerFive === 'no' || answerFive === 'n') {
-    //console.log('Pass');
     score++;
     alert('You are correct, though she hopes to someday be certified and work on a live aboard, she is currently only PADI Advanced Open Water.');
-    //console.log(score);
-    //alert('Your score is ' + score);
   }
 }
 answerFive();
@@ -77,7 +66,7 @@ answerFive();
 function favBooks() {
   let favBooks = ['the overstory', 'braiding sweetgrass', 'grapes of wrath', 'harry potter', 'the goldfinch', 'all the light we cannot see'];
   for (let i = 0; i < 6; i++) {
-    let favBookGuess = prompt('Guess one of my favorite books!').toLowerCase();
+    let favBookGuess = prompt('Guess one Stephs favorite books!').toLowerCase();
     for (let j = 0; j < favBooks.length; j++) {
       if (favBookGuess === favBooks[j]) {
         alert('Yes! If you have not read it, you must!');
@@ -91,7 +80,7 @@ function favBooks() {
 favBooks();
 
 function guessingGame() {
-  let userAnswer = prompt('How old am I?');
+  let userAnswer = prompt('How old is Steph?');
   let correctAnswer = 33;
   let attempts = 4;
 
@@ -101,20 +90,20 @@ function guessingGame() {
     }
 
     if (userAnswer === correctAnswer) {
-      userAnswer = prompt('Wow, I guess I really am looking my age these days.');
+      userAnswer = prompt('Wow, I guess she is really looking her age these days.');
       score++;
       break;
     }
 
     else if (userAnswer > correctAnswer) {
-      userAnswer = prompt('Wow! That is rude! I am not that old!');
+      userAnswer = prompt('Wow! That is rude! She is not that old!');
 
     } else if (userAnswer < correctAnswer) {
-      userAnswer = prompt('Awww, you are so sweet, but I am older than that.');
+      userAnswer = prompt('Awww, you are so sweet, but she is older than that.');
     }
 
     if (i === 4) {
-      alert('You tried, but failed. I am actually 33.');
+      alert('You tried, but failed. She is actually 33.');
     }
   }
 }
